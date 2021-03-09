ec0f39ce5bab84b85659a57235234d868ddb6f51
SheaW84 <sheawilsn@gmail.com>
2021-03-08 21:52:31 -0500

    c++ basics

---
+++ FirstLessons/TripleX.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+//This is the start of the triple x program exercise
+
+int main()
+{
+
+    //Print welcom messages to the terminal
+    std::cout << "You are a demon slayer faced a with demon with set hit points";
+    std::cout << std::endl;
+    std::cout << "Enter the right hit points to slay the demon...";
+
+    // Declare 3 numbers in the code
+    const int CodeA = 4;
+    const int CodeB = 8;
+    const int CodeC = 7;
+
+
+/* this is multiline comment*/
+
+    const int CodeSum = CodeA + CodeB + CodeC;
+
+    const int CodeProduct = CodeA * CodeB * CodeC;
+
+    std::cout << std::endl;
+    std::cout << "There are 3 numbers in the code" << std::endl;
+    std::cout << "The codes add up to " <<CodeSum << std::endl;
+    std::cout << "The product of those same numbers equal to "<<CodeProduct <<std::endl;
+
+    int PlayerGuess;
+
+    return 0;
+}
